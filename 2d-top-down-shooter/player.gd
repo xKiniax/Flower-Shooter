d88b3d06370f96a3d469104b5c96475ef33f92d4
xKiniax <erin.rogalska@gmail.com>
2025-03-31 19:01:14 +0100

    restart button

--- 2d-top-down-shooter/player.gd
+++ 2d-top-down-shooter/player.gd
@@ -10,5 +10,11 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("restart"):
-		get_tree().reload_current_scene()
+		restart()
+	if dead:
+		return
 		
+	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() * PI/2.0
+		
+func restart():
+	get_tree().reload_current_
