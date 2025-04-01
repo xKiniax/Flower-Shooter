6b6dfaf093ed93865553a60856ab0c9eedcefd8b
xKiniax <erin.rogalska@gmail.com>
2025-04-01 11:19:01 +0100

    Player movement working

--- 2d-top-down-shooter/player.gd
+++ 2d-top-down-shooter/player.gd
@@ -15,6 +15,35 @@ func _process(delta: float) -> void:
 		return
 		
 	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() * PI/2.0
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+func _physics_process(delta: float) -> void:
+	if dead:
+		return
+	var move_dir = Input.get_vector("mover_left","move_right", "move_up", "move_down")
+	velocity = move_dir * move_speed
+	move_and_slide()
+	
+func kill():
+	if dead:
+		return
+	dead = true
+	$DeathSound.play()
+	$Graphics/Dead.show()
+	$Graphics/Alive.hide()
+	$CanvasLayer/DeathScreen.show()
+	z_index = -1
+	
 		
 func restart():
 	get_tree().reload_current_
+	
+func shoot():
+	$MuzzleFlash.show()
+	$MuzzleFlash/Timer.start()
+	$ShootSound.play()
+	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider().has_method("kill"):
+		ray_cast_2d.get_collider().kill()
+		
+	
