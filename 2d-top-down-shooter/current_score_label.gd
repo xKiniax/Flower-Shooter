b0f18a444613ffa45ff93af9912ab7fa010fadb8
xKiniax <erin.rogalska@gmail.com>
2025-04-16 18:02:43 +0100

    background for start screen but score still not working

--- 2d-top-down-shooter/current_score_label.gd
+++ 2d-top-down-shooter/current_score_label.gd
@@ -3,5 +3,6 @@ extends Label
 var default_text = "Flowers Flourished: "
 
 func _process(delta):
-	var text = str(default_text, str(Score.current_score))
-	self.text = (text)
+	var _text = default_text + str(Score.current_score)
+	self.text = text
+	print("counting...")
